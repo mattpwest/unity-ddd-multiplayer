cc6b68aca6fe80ae6015fcb6826ef5ca560bf1ba
Matt Van Der Westhuizen <matt.vanderwest@entelect.co.za>
2017-04-01 13:25:02 +0200

    Players can now join the board.

--- TicTacToeDomain/Domain/Board.cs
+++ TicTacToeDomain/Domain/Board.cs
@@ -1,16 +1,49 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace TicTacToe.Domain {
     public class Board {
 
+        private IDictionary<string, Player> players;
+        private Tile[] tiles;
+
         public RuleSet RuleSet { get; private set; }
 
-        public Board(RuleSet ruleSet) {
+        public IEnumerable<Player> Players => this.players.Values;
+
+        public bool ReadyToStart => this.players.Count >= this.RuleSet.MinPlayers;
+
+        private Board() {
+            this.players = new Dictionary<string, Player>();
+        }
+
+        public Board(RuleSet ruleSet) : this() {
             if (ruleSet == null) {
                 throw new ArgumentNullException("rules are required");
             }
 
             this.RuleSet = ruleSet;
+
+            tiles = new Tile[this.RuleSet.BoardWidth * this.RuleSet.BoardHeight];
+            for (int i = 0; i < this.tiles.Length; i++) {
+                tiles[i] = new Tile();
+            }
+        }
+
+        public Tile GetTile(int x, int y) {
+            return this.tiles[y * this.RuleSet.BoardWidth + x];
+        }
+
+        public void Join(Player player) {
+            if (this.players.Count == this.RuleSet.MaxPlayers) {
+                throw new InvalidOperationException("Cannot join - board is full");
+            }
+
+            if (this.players.ContainsKey(player.Name)) {
+                throw new InvalidOperationException("Same player may not join again");
+            }
+
+            this.players.Add(player.Name, player);
         }
     }
 }
--- TicTacToeDomainTest/Domain/BoardTest.cs
+++ TicTacToeDomainTest/Domain/BoardTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
 
 namespace TicTacToe.Domain {
     [TestClass]
@@ -16,6 +17,67 @@ namespace TicTacToe.Domain {
             Board board = new Board(RuleSet.Classic);
 
             Assert.IsNotNull(board.RuleSet);
+        }
+
+        [TestMethod]
+        public void TestBoardCreatesCorrectSizeWithTiles() {
+            var classic = RuleSet.Classic;
+            var board = new Board(classic);
+
+            for (int x = 0; x < classic.BoardWidth; x++) {
+                for (int y = 0; y < classic.BoardHeight; y++) {
+                    Assert.IsNotNull(board.GetTile(x, y));
+                }
+            }
+        }
+
+        [TestMethod]
+        public void TestPlayerJoin() {
+            var classic = RuleSet.Classic;
+            var board = new Board(classic);
+            var player = new Player("Player 1");
+
+            board.Join(player);
+
+            Assert.IsTrue(board.Players.Contains(player));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void TestNoDuplicatePlayers() {
+            var board = new Board(RuleSet.Classic);
+            var player = new Player("Player 1");
+
+            board.Join(player);
+            board.Join(player);
+        }
+
+        [TestMethod]
+        public void TestGameReadyWhenMinPlayersJoined() {
+            var board = new Board(RuleSet.Classic);
+
+            for (int i = 1; i <= RuleSet.Classic.MinPlayers; i++) {
+                var player = new Player($"Player {i}");
+
+                board.Join(player);
+            }
+
+            Assert.IsTrue(board.ReadyToStart);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void TestPlayerCanNotJoinGameThatIsFull() {
+            var board = new Board(RuleSet.Classic);
+
+            for (int i = 1; i <= RuleSet.Classic.MaxPlayers; i++) {
+                var player = new Player($"Player {i}");
+
+                board.Join(player);
+            }
+
+            var playerThatCanNotJoin = new Player("Player Can Not Join");
+            board.Join(playerThatCanNotJoin);
         }
     }
 }
